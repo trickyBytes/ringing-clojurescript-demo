c34339ce0b5880bd90b40fba3915231f2e7bda76
trickyBytes <richard.tiffin@akoolla.com>
2017-09-28 15:40:36 +0100

    Experimentig with clojure/clojurescript and ring

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject my-project "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.908"]
+                 [ring "1.6.2"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main my-project.server
+  :ring {:handler my-project.server/app})
+
